cf862015fb9bb5bc2a99ae9b6c703ef325f5237b
Madina <madinatussupova0820@gmail.com>
2023-04-05 09:29:03 +0600

    get requests

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.listen(3000, () => console.log(`Listening on port ${PORT}!`))
+const friends = [
+	{
+		id: 0,
+		name: "Jay",
+	},
+	{
+		id: 1,
+		name: "Jake",
+	},
+	{
+		id: 2,
+		name: "Mark",
+	},
+];
 
-app.get('/', (req, res) => {
-	res.send('Hello!')
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
+
+app.get('/friends', (req, res) => {
+	res.json(friends);
+})
+
+app.get('/friends/:friendId', (req, res) => {
+	const friendId = +req.params.friendId;
+	const friend = friends[friendId]
+
+	if(friend) {
+		res.json(friend);
+	} else {
+		res.status(404).json({
+			error: "Friend was not found! :("
+		})
+	}
 })
 
 app.get('/message', (req, res) => {
