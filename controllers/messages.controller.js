a3f6fd6a2876fcda4c4d769ce45dafdb750a49be
Madina <madinatussupova0820@gmail.com>
2023-04-07 14:53:10 +0600

    mvc add controllers

---
+++ controllers/messages.controller.js
@@ -0,0 +1,12 @@
+function getMessages(req, res) {
+	res.send('Nice to meet you!');
+}
+
+function postMessage(req, res) {
+	console.log('Updating messages');
+}
+
+module.exports = {
+	getMessages,
+	postMessage
+}
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const messagesController = require('./controllers/messages.controller')
+
+const friendsController = require('./controllers/friends.controller')
+
 const friends = [
 	{
 		id: 0,
@@ -29,43 +33,12 @@ app.use((req, res, next) => {
 
 app.use(express.json())
 
-app.post('/friends', (req, res) => {
-	if(!req.body.name) {
-		return res.status(400).json({
-			error: 'Missing friend name! :('
-		})
-	}
+app.post('/friends', friendsController.postFriend)
 
-	const newFriend = {
-		name: req.body.name,
-		id: friends.length,
-	}
+app.get('/friends', friendsController.getFriends)
 
-	friends.push(newFriend);
-	res.json(newFriend);
-})
+app.get('/friends/:friendId', friendsController.getFriendById)
 
-app.get('/friends', (req, res) => {
-	res.json(friends);
-})
+app.get('/messages', messagesController.getMessages)
 
-app.get('/friends/:friendId', (req, res) => {
-	const friendId = +req.params.friendId;
-	const friend = friends[friendId]
-
-	if(friend) {
-		res.json(friend);
-	} else {
-		res.status(404).json({
-			error: 'Friend was not found! :('
-		})
-	}
-})
-
-app.get('/message', (req, res) => {
-	res.send('Nice to meet you!')
-})
-
-app.post('/message', (req, res) => {
-	console.log("Updating message...");
-})
+app.post('/messages', messagesController.postMessage)
