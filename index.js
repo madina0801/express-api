aec58be614a800471e5d3be82584f11d6754cd02
Madina <madinatussupova0820@gmail.com>
2023-04-10 13:55:21 +0600

    friends router

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
+const friendsRouter = require('./routes/friends.router');
+
 const PORT = 3000;
 
 const messagesController = require('./controllers/messages.controller')
-
-const friendsController = require('./controllers/friends.controller')
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
 
@@ -18,15 +18,6 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-const friendsRouter = express.Router();
 
-friendsRouter.post('/', friendsController.postFriend);
-friendsRouter.get('/', friendsController.getFriends);
-friendsRouter.get('/:friendId', friendsController.getFriendById);
 app.use('/friends', friendsRouter);
-
-
-
-app.get('/messages', messagesController.getMessages)
-
-app.post('/messages', messagesController.postMessage)
+app.use('/messages', messagesRouter);
