f90b438dd2993c929c932f84189c185b017f7ca6
Madina <madinatussupova0820@gmail.com>
2023-04-12 12:47:48 +0600

    layouts

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
 function getMessages(req, res) {
-	res.sendFile(path.join(__dirname, '..', 'public', 'images','friends.jpg'));
+	res.render('messages', {
+		title: "Messages to my friends!",
+		friend: "Soobin",
+	})
+	// res.sendFile(path.join(__dirname, '..', 'public', 'images','friends.jpg'));
 	// res.send('Nice to meet you!');
 }
 
